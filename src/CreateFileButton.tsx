4f822694312aad089c982c1159e586b13966ccaf
thalathalaylah <fragment.vertex@gmail.com>
2019-06-09 18:52:36 +0900

    [add] ボタンを押すとファイルが生成される
    [tmp] testディレクトリを管理しない

--- src/CreateFileButton.tsx
+++ src/CreateFileButton.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Remote } from 'electron';
 import uuid from 'uuid';
+import fsType from 'fs';
+import pathType from 'path';
 
 declare global {
   interface Window {
@@ -11,14 +13,26 @@ declare global {
 
 const remote: Remote = window.require('electron').remote;
 
-function clicked() {
+const fs: typeof fsType = remote.require('fs');
+const path: typeof pathType = remote.require('path');
+
+const testDir = path.join(remote.app.getAppPath(), 'test/');
+
+function createFile() {
   const filename = uuid.v4() + '.txt';
+  if (!fs.existsSync(testDir)) {
+    fs.mkdirSync(testDir);
+  }
+
+  const filePath = testDir + filename;
+  fs.closeSync(fs.openSync(filePath, 'w'));
+
   console.log('created: ' + filename);
 }
 
 export const CreateFileButton = () => {
   return (
-    <button className="CreateFileButton" onClick={clicked}>
+    <button className="CreateFileButton" onClick={createFile}>
       Create file
     </button>
   );
